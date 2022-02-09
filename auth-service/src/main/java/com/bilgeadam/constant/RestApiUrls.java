f163802444061bb932a152a3683eee89f88d243a
Melih DUMANLI <melihdumanli@hotmail.com>
2022-02-09 11:41:33 +0300

    URL's added.

---
+++ auth-service/src/main/java/com/bilgeadam/constant/RestApiUrls.java
@@ -0,0 +1,29 @@
+package com.bilgeadam.constant;
+
+public class RestApiUrls {
+    /** Rest API Service Versions
+    */
+    public static final String VERSION = "v1";
+
+    /** Writing map lists in Controller classes
+    */
+    public static final String PROFILE = "/profile";
+    public static final String USER = "/user";
+
+    /** Defining General requests
+    */
+
+    public static final String SAVE = "/save";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+    public static final String FINDALL = "/findall";
+    public static final String GETALL = "/getall";
+    public static final String FINDBYID = "/findbyid";
+    public static final String FINDBYNAME = "/findbyname";
+
+    /** Defining request URL's for Controller classes
+    */
+
+    public static final String DOLOGIN = "/dologin";
+    public static final String REGISTER = "/register";
+}
